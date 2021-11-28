c4abc906de74d002689fe5bdca8fbd50278675c6
Samko Yun <samko.yun@unimelb.edu.au>
2021-11-28 23:36:07 +1100

    typo fix

--- terraform/modules/networks/main.tf
+++ terraform/modules/networks/main.tf
@@ -120,7 +120,7 @@ resource "aws_route_table" "main_private_rt" {
     vpc_id = "${aws_vpc.main.id}"
     route {
         cidr_block = "0.0.0.0/0"
-        nat_gateway_id = aws_internet_gateway.main_nat.id
+        nat_gateway_id = aws_nat_gateway.main_nat.id
     }
 
     tags = {
