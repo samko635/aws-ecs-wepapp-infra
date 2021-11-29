c0a51304ca2f35a4036dd28411a73f9a51643e29
Samko Yun <samko.yun@unimelb.edu.au>
2021-11-29 23:15:11 +1100

    adjusting min capacity to be same as default desired task

--- terraform/modules/autoscaling/main.tf
+++ terraform/modules/autoscaling/main.tf
@@ -3,7 +3,7 @@
 ####################################
 resource "aws_appautoscaling_target" "ecs_nginx_webapp_target" {
   max_capacity = 10
-  min_capacity = 1
+  min_capacity = 3
   resource_id = "service/${var.ecs_cluster.name}/${var.ecs_service.name}"
   scalable_dimension = "ecs:service:DesiredCount"
   service_namespace = "ecs"
