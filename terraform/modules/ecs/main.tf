4d232f3c0ad025d6478a523c83f46e5a78babd57
Samko Yun <samko.yun@unimelb.edu.au>
2021-11-29 12:22:23 +1100

    remove IAM

--- terraform/modules/ecs/main.tf
+++ terraform/modules/ecs/main.tf
@@ -32,7 +32,7 @@ resource "aws_ecs_service" "nginx_webapp" {
   cluster         = aws_ecs_cluster.webapps_cluster.id
   task_definition = aws_ecs_task_definition.nginx_demo.arn
   desired_count   = 3
-  iam_role        = aws_iam_role.ecs_svc_role.arn
+  # iam_role        = aws_iam_role.ecs_svc_role.arn
   # depends_on      = [aws_iam_role_policy.foo]
 
   network_configuration {
@@ -77,29 +77,29 @@ resource "aws_ecs_task_definition" "nginx_demo" {
 }
 
 # IAM -----------------------
-resource "aws_iam_role" "ecs_svc_role" {
-  name = "nginx-webapp-ecs-role"
+# resource "aws_iam_role" "ecs_svc_role" {
+#   name = "nginx-webapp-ecs-role"
 
-  assume_role_policy = <<EOF
-{
-    "Version": "2008-10-17",
-    "Statement": [{
-        "Sid": "",
-        "Effect": "Allow",
-        "Principal": {
-            "Service": "ecs.amazonaws.com"
-        },
-        "Action": "sts:AssumeRole"
-    }]
-}
-EOF
-}
+#   assume_role_policy = <<EOF
+# {
+#     "Version": "2008-10-17",
+#     "Statement": [{
+#         "Sid": "",
+#         "Effect": "Allow",
+#         "Principal": {
+#             "Service": "ecs.amazonaws.com"
+#         },
+#         "Action": "sts:AssumeRole"
+#     }]
+# }
+# EOF
+# }
 
 # Attach AmazonEC2ContainerServiceRole policy to the new role
-resource "aws_iam_role_policy_attachment" "ecs_svc_role_policy" {
-  role       = "${aws_iam_role.ecs_svc_role.name}"
-  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonEC2ContainerServiceRole"
-}
+# resource "aws_iam_role_policy_attachment" "ecs_svc_role_policy" {
+#   role       = "${aws_iam_role.ecs_svc_role.name}"
+#   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonEC2ContainerServiceRole"
+# }
 
 # resource "aws_cloudwatch_log_group" "svc" {
 #   count = "${length(var.log_groups)}"
